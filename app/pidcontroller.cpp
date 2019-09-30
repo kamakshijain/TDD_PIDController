3bfcee35cd7dec2afa4f01f5cc28aef639d9a206
Kamakshi <kamakshi@terpmail.umd.edu>
2019-09-30 10:58:55 -0400

    Added Constructor Stub

--- app/pidcontroller.cpp
+++ app/pidcontroller.cpp
@@ -8,28 +8,37 @@
 
 PIDController::PIDController() {
 }
+
 PIDController::PIDController(double kp, double ki, double kd) {
 }
+
 PIDController::~PIDController() {
 }
+
 double PIDController::ComputeVelocity(double targetSetpoint, double actual) {
   return 0.0;
 }
+
 double PIDController::RestrictVelocityRate(double velocityRate) {
   return 0.0;
 }
+
 std::vector<double> PIDController::GetPIDGains() {
   std::vector<double> t{0, 0, 0};
   return t;
 }
+
 void PIDController::SetPIDGains(double kp, double ki, double kd) {
 }
+
 std::vector<double> PIDController::GetPIDErrors() {
   std::vector<double> t{0, 0, 0};
   return t;
 }
+
 void PIDController::ResetPIDErrors() {
 }
+
 void PIDController::DisplayPIDInfo() {
 }
 
