fd96120aece6eab67d42238c72af232af79c8032
Kamakshi <kamakshi@terpmail.umd.edu>
2019-09-29 21:24:17 -0400

    Added stub implementation

--- app/pidcontroller.cpp
+++ app/pidcontroller.cpp
@@ -6,3 +6,30 @@
 #include <iostream>
 
 #include "pidcontroller.hpp"
+
+
+double PIDController::ComputeVelocity(double targetSetpoint, double actual) {
+  return 0.0;
+}
+
+double PIDController::RestrictVelocityRate(double velocityRate) {
+  return 0.0;
+}
+
+std::vector<double> PIDController::GetPIDGains() {
+  return std::vector<double> t{0,0,0};
+}
+
+void PIDController::SetPIDGains(double kp, double ki, double kd){
+}
+
+std::vector<double> PIDController::GetPIDErrors() {
+  return std::vector<double> t{0,0,0};
+}
+
+void PIDController::ResetPIDErrors() {
+}
+
+void PIDController::DisplayPIDInfo() {
+}
+
